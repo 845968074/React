bff239f8d0b19b49bd49926d92cdff443db0e880
845968074 <845968074@qq.com>
2016-08-05 16:01:58 +0800

    nest-react

---
+++ demo1/main.js
@@ -0,0 +1,23 @@
+class Firstreact extends React.Component {
+    render() {
+        return (<div className="firstreact">
+            <h1>Hello world</h1>
+        </div>);
+    }
+}
+class Secondreact extends React.Component
+{
+    render()
+    {
+        return(
+            <div className="secondreact">
+                <Firstreact/>
+                <h2>北京欢迎你！</h2>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <Secondreact />,
+    document.getElementById("content")
+);
